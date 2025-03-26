5b550c2703fe9ac27e3bd8015daad3bd2c3f61b4
HatimHajjioui <hhajjioui@cerebotani.it>
2025-03-26 12:40:15 +0100

    inserimento home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -44,13 +44,7 @@
                   link
                 ></v-list-item>
 
-                <v-divider class="my-2"></v-divider>
 
-                <v-list-item
-                  color="grey-lighten-4"
-                  title="Refresh"
-                  link
-                ></v-list-item>
               </v-list>
             </v-sheet>
           </v-col>
